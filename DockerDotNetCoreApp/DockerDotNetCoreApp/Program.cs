6bebcd0f4016d37f4528a73c008493ab228f3b15
Philippe <pvlerick@gmail.com>
2017-02-24 07:56:54 +0100

    .NET Core ConsoleApp

---
+++ DockerDotNetCoreApp/DockerDotNetCoreApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace DockerDotNetCoreApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var content = JsonConvert.DeserializeObject<Content>(File.ReadAllText("quotes.json"));
+
+            var r = new Random().Next(content.Quotes.Length);
+
+            Console.WriteLine(content.Quotes[r]);
+        }
+
+        public class Content
+        {
+            public string[] Quotes { get; set; }
+        }
+    }
+}
